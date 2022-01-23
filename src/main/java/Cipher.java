861144ab73007a9f449f5ab7c60c1b4421a1a810
TrekHub <teddyomondi001@gmail.com>
2022-01-23 16:12:39 +0300

    adding the cipher class

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,21 @@
+public class Cipher {
+
+    String inputString;
+    int cipherKey;
+
+    public String getInputString() {
+        return inputString;
+    }
+
+    public void setInputString(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public int getCipherKey() {
+        return cipherKey;
+    }
+
+    public void setCipherKey(int cipherKey) {
+        this.cipherKey = cipherKey;
+    }
+}
